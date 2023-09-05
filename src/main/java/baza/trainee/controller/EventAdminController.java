43c9d4e1fa5b15ab6c24366310f94891aff5707a
Korkach Oleksandr <oleksandrkorkachwork@gmail.com>
2023-09-05 13:49:16 +0300

    fixed and improve methods, add converter

--- src/main/java/baza/trainee/controller/EventAdminController.java
+++ src/main/java/baza/trainee/controller/EventAdminController.java
@@ -1,63 +1,47 @@
 package baza.trainee.controller;
 
-import baza.trainee.domain.dto.event.EventDto;
-import baza.trainee.domain.dto.event.EventPreviewDto;
+import baza.trainee.domain.dto.event.EventConverter;
 import baza.trainee.domain.dto.event.EventPublicationDto;
+import baza.trainee.domain.model.Event;
 import baza.trainee.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/admin/events")
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class EventAdminController {
 
     private final EventService eventService;
+    private final EventConverter eventConverter;
 
     /**
-     * @return  A list of event previews.
-     */
-    @GetMapping
-    public ResponseEntity<List<EventPreviewDto>> getEvents() {
-        List<EventPreviewDto> events = eventService.getEvents();
-        return ResponseEntity.ok(events);
-    }
-
-    /**
-     * @param id The event id for looking a certain event
-     * @return An event dto.
-     */
-    @GetMapping("/{id}")
-    public ResponseEntity<EventDto> getEvent(final @PathVariable String id) {
-        EventDto event = eventService.getEventById(id);
-        return ResponseEntity.ok(event);
-    }
-
-    /**
-     * @param event The EventPublicationDto containing information about the event to be created.
+     * @param eventPublicationDto The EventPublicationDto containing information about the event to be created.
      * @return ResponseEntity with an HTTP status code indicating the result of the operation.
      */
-    @PostMapping("/create")
-    public ResponseEntity<?> createEvent(final EventPublicationDto event) {
-        eventService.createEvent(event);
+    @PostMapping("/events")
+    public ResponseEntity<?> createEvent(final EventPublicationDto eventPublicationDto) {
+        Event newEvent = eventConverter.eventPublicationDtoToEvent(eventPublicationDto);
+        eventService.createEvent(newEvent);
         return ResponseEntity.ok().build();
     }
 
     /**
      * Update an existing event identified by its unique identifier.
      * @param id The unique identifier of the event to be updated.
-     * @param updatedEvent The EventPublicationDto containing the updated information for the event.
+     * @param eventPublicationDto The EventPublicationDto containing the updated information for the event.
      * @return ResponseEntity with an HTTP status code indicating the result of the operation.
      */
-    @PostMapping("/{id}/updated")
-    public ResponseEntity<?> updateEvent(final @PathVariable String id, final EventPublicationDto updatedEvent) {
+    @PutMapping("events/{id}")
+    public ResponseEntity<?> updateEvent(final @PathVariable String id, final EventPublicationDto eventPublicationDto) {
+        Event updatedEvent = eventConverter.eventPublicationDtoToEvent(eventPublicationDto);
         eventService.updateEventById(updatedEvent, id);
         return ResponseEntity.ok().build();
     }
@@ -67,9 +51,9 @@ public class EventAdminController {
      * @param id The unique identifier of the event to be deleted.
      * @return ResponseEntity with an HTTP status code indicating the result of the operation.
      */
-    @PostMapping("/events/{id}/delete")
+    @DeleteMapping("/events/{id}")
     public ResponseEntity<?> deleteEvent(final @PathVariable String id) {
         eventService.deleteEventById(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/baza/trainee/controller/EventController.java
+++ src/main/java/baza/trainee/controller/EventController.java
@@ -1,16 +1,18 @@
 package baza.trainee.controller;
 
+import baza.trainee.domain.dto.event.EventConverter;
 import baza.trainee.domain.dto.event.EventDto;
 import baza.trainee.domain.dto.event.EventPreviewDto;
+import baza.trainee.domain.model.Event;
 import baza.trainee.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/events")
@@ -18,14 +20,18 @@ import java.util.List;
 public class EventController {
 
     private final EventService eventService;
+    private final EventConverter eventConverter;
 
     /**
      * @return  A list of event previews.
      */
     @GetMapping
-    public ResponseEntity<List<EventPreviewDto>> getEvents() {
-        List<EventPreviewDto> events = eventService.getEvents();
-        return ResponseEntity.ok(events);
+    public List<EventPreviewDto> getEvents() {
+        List<Event> events = eventService.getEvents();
+
+        return events.stream()
+                .map(eventConverter::eventToEventPreviewDto)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -33,8 +39,8 @@ public class EventController {
      * @return An event dto.
      */
     @GetMapping("/{id}")
-    public ResponseEntity<EventDto> getEvent(final @PathVariable String id) {
-        EventDto event = eventService.getEventById(id);
-        return ResponseEntity.ok(event);
+    public EventDto getEvent(final @PathVariable String id) {
+        Event event = eventService.getEventById(id);
+        return eventConverter.eventToEventDto(event);
     }
 }
