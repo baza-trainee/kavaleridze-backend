d2aa8494a31372e8c5ba16fbdc8c6e53eb1ef575
Korkach Oleksandr <oleksandrkorkachwork@gmail.com>
2023-09-08 13:55:47 +0300

    Test: test for EventController

--- src/test/java/baza/trainee/controller/EventControllerTest.java
+++ src/test/java/baza/trainee/controller/EventControllerTest.java
@@ -1,76 +1,99 @@
 package baza.trainee.controller;
 
-import baza.trainee.domain.dto.event.EventConverter;
 import baza.trainee.domain.dto.event.EventDto;
+import baza.trainee.domain.dto.event.EventPreviewDto;
 import baza.trainee.domain.enums.ContentType;
-import baza.trainee.domain.model.Event;
+import baza.trainee.domain.enums.EventTheme;
 import baza.trainee.service.EventService;
-import org.junit.jupiter.api.BeforeEach;
+import baza.trainee.utils.LoggingService;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(EventController.class)
 public class EventControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private EventService eventService;
 
-    @Mock
-    private EventConverter eventConverter;
-
-    @InjectMocks
-    private EventController eventController;
-
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(eventController).build();
-    }
+    @MockBean
+    private LoggingService logService;
 
     @Test
     public void testGetEvents() throws Exception {
-        List<Event> events = new ArrayList<>();
-        Event event1 = new Event();
-        event1.setId("1");
-        Event event2 = new Event();
-        event2.setId("2");
-        events.add(event1);
-        events.add(event2);
+        List<EventPreviewDto> events = new ArrayList<>();
 
         when(eventService.getEvents()).thenReturn(events);
 
-        mockMvc.perform(get("/events"))
+        mockMvc.perform(get("/events")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(2));
+                .andExpect(jsonPath("$").isArray());
     }
 
     @Test
     public void testGetEvent() throws Exception {
-        String eventId = "1";
-        Event event = new Event();
-        event.setId(eventId);
-        EventDto eventDto = new EventDto(eventId, ContentType.EVENT, "Test Event", "Event Content", null);
+        final LocalDate published = LocalDate.now();
+        final LocalDate begin = LocalDate.of(2023, 9, 3);
+        final LocalDate end = LocalDate.of(2023, 9, 12);
+        final String eventId = "32";
+
+        EventDto event = new EventDto(
+                eventId,
+                ContentType.EVENT,
+                EventTheme.CINEMA,
+                Collections.emptyList(),
+                "cool title",
+                "shortDesc",
+                Collections.emptyList(),
+                "/images/image1.jpeg",
+                published,
+                begin,
+                end);
 
         when(eventService.getEventById(eventId)).thenReturn(event);
-        when(eventConverter.eventToEventDto(event)).thenReturn(eventDto);
 
-        mockMvc.perform(get("/events/{id}", eventId))
+        mockMvc.perform(get("/events/{id}", eventId)
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(eventId))
-                .andExpect(jsonPath("$.title").value("Test Event"))
-                .andExpect(jsonPath("$.content").value("Event Content"));
+                .andExpect(jsonPath("$.id").value(eventId));
+    }
+
+    @Test
+    public void testGetEventsWithNoEvents() throws Exception {
+        when(eventService.getEvents()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/events")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isEmpty());
+    }
+
+    @Test
+    public void testGetEventWithInvalidId() throws Exception {
+        final String invalidEventId = "999";
+
+        when(eventService.getEventById(invalidEventId)).thenReturn(null);
+
+        mockMvc.perform(get("/events/{id}", invalidEventId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
