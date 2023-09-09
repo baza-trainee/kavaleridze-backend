90103b722279b6975fe94949a8c3259bd82e3b05
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-09 19:51:34 +0300

    feat: add Post contract

---
+++ src/main/java/baza/trainee/domain/model/Post.java
@@ -0,0 +1,30 @@
+package baza.trainee.domain.model;
+
+/**
+ * Interface of the web post.
+ *
+ * @author Evhen Malysh
+ */
+public interface Post {
+
+    /**
+     * Getter for ID field.
+     *
+     * @return ID of the Post.
+     */
+    String getId();
+
+    /**
+     * Getter for Title field.
+     *
+     * @return post title.
+     */
+    String getTitle();
+
+    /**
+     * Getter for Description field.
+     *
+     * @return post description.
+     */
+    String getDescription();
+}
