f27b6cc4ff9a2dc9dca47d6dfd413425aa0545f0
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-09 16:14:58 +0300

    add: Event, ContentBlock, EventPublication tests

--- src/test/java/baza/trainee/domain/model/ContentBlockTest.java
+++ src/test/java/baza/trainee/domain/model/ContentBlockTest.java
@@ -6,7 +6,9 @@ import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,30 +16,62 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ContentBlockTest {
 
     private Validator validator;
+    private ContentBlock validBlock;
 
     @BeforeEach
     public void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+        validBlock = new ContentBlock();
+        validBlock.setId("12");
+        validBlock.setBlockType(BlockType.PICTURE_TEXT_BLOCK);
+        validBlock.setOrder(2);
+        validBlock.setColumns(4);
+        validBlock.setPictureLink("https://example.com/banner.jpg");
+        validBlock.setTextContent("text content");
     }
 
     @Test
     public void testContentBlockWithValidData() {
-        final Integer columns = 4;
-        ContentBlock contentBlock = new ContentBlock();
-        contentBlock.setId("12");
-        contentBlock.setBlockType(BlockType.PICTURE_TEXT_BLOCK);
-        contentBlock.setOrder(2);
-        contentBlock.setColumns(columns);
-        contentBlock.setPictureLink("https://example.com/banner.jpg");
-        contentBlock.setTextContent("text content");
+        // given:
+        ContentBlock contentBlock = validBlock;
 
+        // then:
         assertTrue(validator.validate(contentBlock).isEmpty());
     }
 
-    @Test
-    public void testEventWithInvalidData() {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(ints = {-1})
+    public void testEventWithInvalidOrder(Integer order) {
+        // given:
         ContentBlock contentBlock = new ContentBlock();
+        contentBlock.setOrder(order);
+
+        // then:
+        assertFalse(validator.validate(contentBlock).isEmpty());
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(ints = {-1})
+    public void testEventWithInvalidColumns(Integer columns) {
+        // given:
+        ContentBlock contentBlock = new ContentBlock();
+        contentBlock.setColumns(columns);
+
+        // then:
+        assertFalse(validator.validate(contentBlock).isEmpty());
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void testEventWithInvalidColumns(BlockType type) {
+        // given:
+        ContentBlock contentBlock = new ContentBlock();
+        contentBlock.setBlockType(type);
+
+        // then:
         assertFalse(validator.validate(contentBlock).isEmpty());
     }
 }
--- src/test/java/baza/trainee/domain/model/EventTest.java
+++ src/test/java/baza/trainee/domain/model/EventTest.java
@@ -1,52 +1,122 @@
 package baza.trainee.domain.model;
 
-import baza.trainee.domain.enums.ContentType;
-import baza.trainee.domain.enums.EventTheme;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import java.time.LocalDate;
-import java.util.Collections;
+import java.util.stream.Stream;
 
+import static baza.trainee.domain.TestConstraints.OVERSIZED_DESCRIPTION;
+import static baza.trainee.domain.TestConstraints.OVERSIZED_TITLE;
+import static baza.trainee.domain.TestConstraints.UNDERSIZED_DESCRIPTION;
+import static baza.trainee.domain.TestConstraints.UNDERSIZED_TITLE;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EventTest {
 
     private Validator validator;
+    private Event validEvent;
 
     @BeforeEach
-    public void setUp() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
+    void setUp() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+
+        validEvent = new Event();
+        validEvent.setId("123");
+        validEvent.setType("valid-type");
+        validEvent.addTag("tag");
+        validEvent.setTitle("Valid Title");
+        validEvent.setDescription("Valid Description");
+        validEvent.setBannerURI("https://example.com/banner.jpg");
+        validEvent.setBannerPreviewURI("https://example.com/banner-preview.jpg");
+        validEvent.addContentBlock(new ContentBlock());
+        validEvent.setBegin(LocalDate.now());
+        validEvent.setEnd(LocalDate.now().plusDays(1));
     }
 
     @Test
     public void testEventWithValidData() {
-        Event event = new Event();
-        event.setId("123");
-        event.setContentType(ContentType.EVENT);
-        event.setEventTheme(EventTheme.PAINTING);
-        event.setTags(Collections.singletonList("tag"));
-        event.setTitle("Valid Title");
-        event.setShortDescription("Valid Description");
-        event.setBannerImage("https://example.com/banner.jpg");
-        event.setBannerImagePreview("https://example.com/banner-preview.jpg");
-        event.setContentBlocks(Collections.singletonList(new ContentBlock()));
-        event.setBegin(LocalDate.now());
-        event.setEnd(LocalDate.now().plusDays(1));
-        event.setCreated(LocalDate.now());
-        event.setUpdated(LocalDate.now());
+        // given:
+        Event event = validEvent;
 
+        // then:
         assertTrue(validator.validate(event).isEmpty());
     }
 
-    @Test
-    public void testEventWithInvalidData() {
-        Event event = new Event();
-        assertFalse(validator.validate(event).isEmpty());
+    @ParameterizedTest
+    @NullAndEmptySource
+    @MethodSource
+    public void testEventWithInvalidTitle(String title) {
+        // given:
+        Event event = validEvent;
+        event.setTitle(title);
+
+        // when:
+        var constraintViolations = validator.validate(event);
+
+        // then:
+        assertFalse(constraintViolations.isEmpty());
+    }
+
+    private static Stream<Arguments> testEventWithInvalidTitle() {
+        return Stream.of(
+                Arguments.of(UNDERSIZED_TITLE),
+                Arguments.of(OVERSIZED_TITLE)
+        );
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @MethodSource
+    public void testEventWithInvalidDescription(String description) {
+        // given:
+        Event event = validEvent;
+        event.setDescription(description);
+
+        // when:
+        var constraintViolations = validator.validate(event);
+
+        // then:
+        assertFalse(constraintViolations.isEmpty());
+    }
+
+    private static Stream<Arguments> testEventWithInvalidDescription() {
+        return Stream.of(
+                Arguments.of(UNDERSIZED_DESCRIPTION),
+                Arguments.of(OVERSIZED_DESCRIPTION)
+        );
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @MethodSource
+    public void testEventWithInvalidDates(LocalDate date) {
+        // given:
+        Event event = validEvent;
+        event.setBegin(date);
+        event.setEnd(date);
+
+        // when:
+        var constraintViolations = validator.validate(event);
+
+        // then:
+        assertFalse(constraintViolations.isEmpty());
+    }
+
+    private static Stream<Arguments> testEventWithInvalidDates() {
+        return Stream.of(
+                Arguments.of(LocalDate.now().minusDays(1))
+        );
     }
 }
