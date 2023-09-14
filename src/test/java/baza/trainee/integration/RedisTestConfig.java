305dc808f348b18a9a44c3f100ec8a3e9686b085
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-14 17:06:38 +0300

    test: update redis configs

--- src/test/java/baza/trainee/integration/RedisTestConfig.java
+++ src/test/java/baza/trainee/integration/RedisTestConfig.java
@@ -4,9 +4,6 @@ import baza.trainee.domain.model.Article;
 import baza.trainee.domain.model.Event;
 import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * Jedis test configurations.
@@ -21,18 +18,4 @@ import org.springframework.data.redis.core.RedisTemplate;
                 Event.class,
                 Article.class
         })
-public class RedisTestConfig {
-
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory() {
-//        return new JedisConnectionFactory();
-//    }
-//
-//    @Bean
-//    RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        return template;
-//    }
-
-}
+public class RedisTestConfig { }
