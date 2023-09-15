8f157569cb2591d70abd2395ec6643b4c51595e1
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-15 11:20:21 +0300

    test: fix tests and comment it

--- src/test/java/baza/trainee/integration/EventServiceImplTest.java
+++ src/test/java/baza/trainee/integration/EventServiceImplTest.java
@@ -3,6 +3,7 @@ package baza.trainee.integration;
 import baza.trainee.domain.dto.event.EventPublication;
 import baza.trainee.domain.mapper.EventMapper;
 import baza.trainee.domain.model.Event;
+import baza.trainee.exceptions.custom.EntityNotFoundException;
 import baza.trainee.service.EventService;
 import baza.trainee.service.SearchService;
 import org.junit.jupiter.api.Assertions;
@@ -14,25 +15,25 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.time.LocalDate;
 
-@Import({ EventTestDataInitializer.class })
+import static org.junit.jupiter.api.Assertions.*;
+
+@Import({EventTestDataInitializer.class})
 class EventServiceImplTest extends AbstractIntegrationTest {
 
     @Autowired
     private EventService eventService;
-    
+
     @Autowired
     private EventMapper mapper;
-    
+
     @MockBean
     private SearchService searchService;
 
     /*
      * TODO: fix code style.
-     * 
+     *
      * Separate code blocks by stages.
      */
     @Test
@@ -44,17 +45,26 @@ class EventServiceImplTest extends AbstractIntegrationTest {
         Page<Event> result = eventService.getAll(pageable);
 
         // when:
-        int numberPage = result.getTotalPages();
+        int numberOfElements = result.getNumberOfElements();
+        int numberPage = result.getTotalPages();                // This check is redundant.
         int numberEvents = (int) result.getTotalElements();
 
         // then:
-        assertEquals(2, numberPage);
+        assertEquals(10, numberOfElements);
         assertEquals(20, numberEvents);
+
+        /*
+         * You can check fields that are set by DB, like ID or date of record creation.
+         */
+        for (var event : result) {
+            assertNotNull(event.getId());
+            assertNotNull(event.getCreated());
+        }
     }
 
     /*
      * TODO: fix naming.
-     * 
+     *
      * Add Test postfix to methods names, like 'getByIdTest()'.
      */
     @Test
@@ -63,21 +73,21 @@ class EventServiceImplTest extends AbstractIntegrationTest {
 
         /*
          * TODO: fix dates as in update method below.
-         * 
+         *
          * LocalDate.ofEpochDay(long number) << But you try to pass '2023-5-25' like a
          * date.
-         * 
+         *
          * Assuming that this timestamp is in seconds: 2023525
          * GMT: Saturday, January 24, 1970 10:05:25 AM
          * Your time zone: Saturday, January 24, 1970 1:05:25 PM GMT+03:00
          * Relative: 54 years ago
-         * 
+         *
          * And do not try to pass concrete date.
          * Checks is relative to date of tests execution.
          * If you provide Sep 14 2023 in date that checked
          * by {@link FutureOrPresent} annotation,
          * in Sep 15 2023 your tests become invalid.
-         * 
+         *
          */
         // given:
         EventPublication eventPublication = new EventPublication(
@@ -157,6 +167,12 @@ class EventServiceImplTest extends AbstractIntegrationTest {
         eventService.deleteEventById(id);
 
         // then:
-        Assertions.assertNotNull(eventService.getById(id));
+        /*
+         * You throw an exception in your method when the Event is not found.
+         * And, as we want, it is not found.
+         * So, you should check that an exception is thrown in assertions.
+         */
+        assertThrows(EntityNotFoundException.class, () -> eventService.getById(id),
+                "Event with `ID: " + id + "` was not found!");
     }
 }
