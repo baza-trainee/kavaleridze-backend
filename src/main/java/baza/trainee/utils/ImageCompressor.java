257081e92c730dc97fe172b8619cc1a94a6ea9aa
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-09-20 20:29:13 +0300

    refactor: javadoc

--- src/main/java/baza/trainee/utils/ImageCompressor.java
+++ src/main/java/baza/trainee/utils/ImageCompressor.java
@@ -23,7 +23,7 @@ public class ImageCompressor {
      * @param targetWidth The target width for the compressed image.
      * @param quality     The quality level for the compressed image (a value between 0.0 and 1.0).
      * @return A compressed MultipartFile representing the compressed image.
-     * @throws IOException If an I/O error occurs while processing the input file.
+     * @throws ImageCompressionException If an I/O error occurs while processing the input file.
      */
     public static MultipartFile compress(final MultipartFile inputFile, final int targetWidth, final float quality) {
         try (InputStream inputStream = inputFile.getInputStream()) {
