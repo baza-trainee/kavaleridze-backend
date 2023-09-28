c29da498fd94686a8ac67cae52f7bfd07a763eb1
Anatolii_Omelchenko <omelchenko20@gmail.com>
2023-09-28 18:04:46 +0300

    chore: added Java docks for MuseumData (controller, service, model)

--- src/main/java/baza/trainee/controller/admin/MuseumDataAdminController.java
+++ src/main/java/baza/trainee/controller/admin/MuseumDataAdminController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
+/**
+ * A REST controller class for managing museum data in the admin API.
+ * This controller handles operations such as adding and updating museum data.
+ */
 @RestController
 @RequestMapping("/api/admin/museum_data")
 @RequiredArgsConstructor
@@ -18,12 +22,24 @@ public class MuseumDataAdminController {
 
     private final MuseumDataService museumDataService;
 
+    /**
+     * Adds new museum data.
+     *
+     * @param museumData The MuseumData object containing new contacts data of museum.
+     * @return The MuseumData object that has been added.
+     */
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public MuseumData addData(@RequestBody MuseumData museumData) {
         return museumDataService.add(museumData);
     }
 
+    /**
+     * Updates existing museum data.
+     *
+     * @param museumData The MuseumData object containing updated contacts data of museum.
+     * @return The MuseumData object that has been updated.
+     */
     @PutMapping
     public MuseumData updateData(@RequestBody MuseumData museumData) {
         return museumDataService.update(museumData);
