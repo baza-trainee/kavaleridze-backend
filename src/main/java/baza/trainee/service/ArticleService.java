aea35dfa61d488757e09669d95723bac78d21f49
Evhen Malysh <malysh.evgeniy@gmail.com>
2023-10-05 11:58:15 +0300

    style: javadoc return type

--- src/main/java/baza/trainee/service/ArticleService.java
+++ src/main/java/baza/trainee/service/ArticleService.java
@@ -1,6 +1,5 @@
 package baza.trainee.service;
 
-import baza.trainee.domain.model.Article;
 import baza.trainee.dto.ArticleResponse;
 
 public interface ArticleService {
@@ -9,7 +8,7 @@ public interface ArticleService {
      * Finds an existing article by given title.
      *
      * @param title title to get an existing article.
-     * @return {@link Article} object containing an existing article with its full content.
+     * @return {@link ArticleResponse} object containing an existing article with its full content.
      */
     ArticleResponse findByTitle(String title);
 }
